82a61117927b505f323df76ffa3ff524b637094f
zc55803903 <549783842@qq.com>
2014-09-15 17:23:47 +0800

    start2

---
+++ src/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    CloseNormal_png : "res/CloseNormal.png",
+    CloseSelected_png : "res/CloseSelected.png",
+    s_ball : "res/ball.png",
+    s_paddle : "res/paddle.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
